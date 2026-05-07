0dfc5ed43634df7fc7ae5242f8f1ad8d8d4cd511
Ankit Vishwakarma <ankvish1102@gmail.com>
2026-05-07 17:32:23 +0530

    fixed route

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,14 +2,14 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "https://interviewiq-ath8.onrender.com",
+    baseURL: "http://localhost:3000",
     withCredentials: true
 })
 
 export async function register({ username, email, password }) {
 
     try {
-        const response = await api.post('https://interviewiq-ath8.onrender.com/api/auth/register', {
+        const response = await api.post('/api/auth/register', {
             username, email, password
         })
 
@@ -27,7 +27,7 @@ export async function login({ email, password }) {
 
     try {
 
-        const response = await api.post("https://interviewiq-ath8.onrender.com/api/auth/login", {
+        const response = await api.post("/api/auth/login", {
             email, password
         })
 
@@ -42,7 +42,7 @@ export async function login({ email, password }) {
 export async function logout() {
     try {
 
-        const response = await api.get("https://interviewiq-ath8.onrender.com/api/auth/logout")
+        const response = await api.get("/api/auth/logout")
 
         return response.data
 
@@ -55,7 +55,7 @@ export async function getMe() {
 
     try {
 
-        const response = await api.get("https://interviewiq-ath8.onrender.com/api/auth/get-me")
+        const response = await api.get("/api/auth/get-me")
 
         return response.data
 
