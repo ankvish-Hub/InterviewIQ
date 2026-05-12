6900397a051591900ae24e530224dae979f1c03e
Ankit Vishwakarma <ankvish1102@gmail.com>
2026-05-12 22:28:27 +0530

    fixed route

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: ["http://localhost:5173", "https://interview-iq-three-eta.vercel.app"],
+    origin: ["http://localhost:5173", "https://interview-iq-virid.vercel.app"],
     credentials: true
 }))
 
